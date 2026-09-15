4321a90dcee32fa524f1b5a4d0ebcbe56548ebbe
PignoufCoco <joguetcorentin34@gmail.com>
2026-09-15 14:55:05 +0200

    a

--- mqtt-web/public/logBDD.php
+++ mqtt-web/public/logBDD.php
@@ -1,6 +1,6 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=172.16.89.44;dbname=esp32_db;charset=utf8mb4",
+    "mysql:host=172.16.89.44;dbname=univers_videos;charset=utf8mb4",
     "web_app",
     "MotDePasseFort123!"
 );
