19ce47fe81a9d7ebbfb923697905aefafe1d1de1
PignoufCoco <joguetcorentin34@gmail.com>
2026-09-16 09:52:53 +0200

    a

--- mqtt-web/public/recherche.php
+++ mqtt-web/public/recherche.php
@@ -39,14 +39,14 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Wiki Rick & Morty</title>
+    <title>Wiki Planètes</title>
     <link rel="stylesheet" href="recherche.css">
 
 </head>
 
 <body>
 
-    <h1>Wiki Rick & Morty</h1>
+    <h1>Wiki Planètes</h1>
 
     <form method="GET">
 
